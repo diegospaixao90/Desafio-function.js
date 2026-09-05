00c06e857bce9a3b3bdec31d3b3119ee02ed7ca1
diegosp9090-pilha <diegosp9090@gmail.com>
2026-09-05 17:30:54 -0300

    funcoes parte 1

---
+++ scripts.js
@@ -0,0 +1,43 @@
+const result = document.querySelector('.result');
+
+const playHuman = (humanChoice) => {
+
+    playTheGame(humanChoice, playMachine())
+}
+
+const playMachine = () => {
+    const choices = ['rock', 'paper', 'scissors']
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    return choices[randomNumber]
+
+}
+
+const playTheGame = (humanChoice, machineChoice) => {
+
+    console.log('Humano:', humanChoice);
+    console.log('Máquina:', machineChoice);
+
+    if (humanChoice === machineChoice) {
+        result.src = 'acetis/empate.gif'
+        result.style.width = '200px';
+        result.style.height = '100px';
+    }
+
+    else if ((humanChoice === 'rock' && machineChoice === 'scissors') ||
+        (humanChoice === 'paper' && machineChoice === 'rock') ||
+        (humanChoice === 'scissors' && machineChoice === 'paper')) {
+        result.src = 'acetis/voce ganhou.gif'
+        result.style.width = '200px';
+        result.style.height = '100px';
+
+    }
+
+    else {
+        result.src = 'acetis/perdeu.gif'
+        result.style.width = '200px';
+        result.style.height = '100px';
+    }
+}
+
+
