faad60371ef41280c98dc9493be5af7598e031f7
diegosp9090-pilha <diegosp9090@gmail.com>
2026-09-05 18:16:34 -0300

    atualizacao funcao

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
 const result = document.querySelector('.result');
+const humanScore = document.querySelector("#human-score")
+const machineScore = document.querySelector("#machine-score")
+
+let humanScoreNumber = 0
+let machineScoreNumber = 0
 
 const playHuman = (humanChoice) => {
 
@@ -24,16 +29,21 @@ const playTheGame = (humanChoice, machineChoice) => {
         result.style.height = '100px';
     }
 
-    else if ((humanChoice === 'rock' && machineChoice === 'scissors') ||
+    else if (
+        (humanChoice === 'rock' && machineChoice === 'scissors') ||
         (humanChoice === 'paper' && machineChoice === 'rock') ||
-        (humanChoice === 'scissors' && machineChoice === 'paper')) {
+        (humanChoice === 'scissors' && machineChoice === 'paper')
+    ) {
+        humanScoreNumber++
+        humanScore.innerHTML = humanScoreNumber
         result.src = 'acetis/voce ganhou.gif'
         result.style.width = '200px';
         result.style.height = '100px';
-
     }
 
     else {
+        machineScoreNumber++
+        machineScore.innerHTML = machineScoreNumber
         result.src = 'acetis/perdeu.gif'
         result.style.width = '200px';
         result.style.height = '100px';
